cb762cb510a185bd5aeb4a1361da22ceb660eb58
Lukasz Karluk <julapy@googlemail.com>
2017-09-28 15:03:55 +1000

    UnityVideoWriter start

---
+++ src/UnityVideoWriter.m
@@ -0,0 +1,17 @@
+//
+//  UnityVideoWriter.m
+//  Created by Lukasz Karluk on 28/9/17.
+//  Copyright © 2017 Lukasz Karluk. All rights reserved.
+//
+
+#import "UnityVideoWriter.h"
+#import "VideoWriter.h"
+
+@interface UnityVideoWriter() {
+
+}
+@end
+
+@implementation UnityVideoWriter
+
+@end
