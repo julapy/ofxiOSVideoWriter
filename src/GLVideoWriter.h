f997c710d2dd3b180a8a17a9b7cfed4249a1a974
Lukasz Karluk <julapy@googlemail.com>
2017-10-09 17:45:57 +1100

    removed swizzling - assuming texture cache is always available.

--- src/GLVideoWriter.h
+++ src/GLVideoWriter.h
@@ -7,9 +7,70 @@
 #include "ofMain.h"
 #include "VideoWriter.h"
 
+//-------------------------------------------------------------------------
 class GLVideoWriter {
     
 public:
+
+    //--------------------------------------------------------------------- Shader.
+    class Shader {
+    public:
+        Shader():
+        program(0),
+        vert(0),
+        frag(0),
+        bAllocated(false) {
+            //
+        }
+        
+        GLuint program;
+        GLuint vert;
+        GLuint frag;
+        bool bAllocated;
+    };
+    
+    //--------------------------------------------------------------------- Texture.
+    class Texture {
+    public:
+        Texture():
+        texW(0),
+        texH(0),
+        texT(0),
+        texU(0),
+        texture(0),
+        bExternal(false),
+        bAllocated(false) {
+            //
+        }
+
+        GLint texW;
+        GLint texH;
+        float texT;
+        float texU;
+        GLuint texture;
+        bool bExternal;
+        bool bAllocated;
+    };
+
+    //--------------------------------------------------------------------- Fbo.
+    class Fbo {
+    public:
+        Fbo():
+        fboW(0),
+        fboH(0),
+        framebuffer(0),
+        bAllocated(false) {
+            //
+        }
+        
+        GLint fboW;
+        GLint fboH;
+        GLuint framebuffer;
+        Texture tex;
+        bool bAllocated;
+    };
+    
+    //--------------------------------------------------------------------- GLVideoWriter
     GLVideoWriter();
     ~GLVideoWriter();
     
@@ -18,10 +79,22 @@ public:
     void setFPS(float fps);
     float getFPS();
     
-    void initShader();
-    GLuint compileShader(GLenum type, string source);
-    void bindShader();
-    void unbindShader();
+    void initFbo(Fbo & fbo, int w, int h) const;
+    void killFbo(Fbo & fbo) const;
+    void initFboTexture(Fbo & fbo) const;
+    void initFboTexture(Fbo & fbo, GLuint externalTexture) const;
+    void initTexture(const Texture & tex) const;
+    void killTexture(Texture & tex) const;
+    void bindFbo(const Fbo & fbo) const;
+    void bindDefaultFbo(const Fbo & fbo) const;
+    void beginFbo(const Fbo & fbo) const;
+    void endFbo(const Fbo & fbo) const;
+    
+    void initShader(Shader & shader) const;
+    void killShader(Shader & shader) const;
+    GLuint compileShader(GLenum type, string source) const;
+    void bindShader(const Shader & shader) const;
+    void unbindShader(const Shader & shader) const;
     
     void update();
     void draw(ofRectangle & rect);
@@ -41,16 +114,16 @@ public:
     void end();
     
     VideoWriter * videoWriter;
-    ofFbo fbo;
-    ofFbo fboBGRA;
+    ofFbo fboRGBA;
     
     float startTime;
     int recordFrameNum;
     float recordFPS;
     bool bLockToFPS;
     bool bUseTextureCache;
-    
-    GLuint program;
-    GLuint shaderVert;
-    GLuint shaderFrag;
+
+    Shader shader;
+    Fbo fbo;
+    GLint defaultFrameBuffer;
+
 };
--- src/GLVideoWriter.mm
+++ src/GLVideoWriter.mm
@@ -10,7 +10,14 @@
 #include "ofxiOSEAGLView.h"
 #import "AVFoundationVideoPlayer.h"
 
-//-------------------------------------------------------------------------
+//------------------------------------------------------------------------- Utils.
+int NextPow2(int a){
+    int rval=1;
+    while(rval<a) rval<<=1;
+    return rval;
+}
+
+//------------------------------------------------------------------------- Shaders.
 #define STRINGIFY(x) #x
 
 static string swizzleVertexShader = STRINGIFY(
@@ -77,10 +84,12 @@ void GLVideoWriter::setup(int videoWidth, int videoHeight, string filePath) {
     videoWriter.context = [ofxiOSEAGLView getInstance].context; // TODO - this should probably be passed in with init.
     videoWriter.enableTextureCache = YES; // TODO - this should be turned on by default when it is working.
     
-    initShader();
+    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &defaultFrameBuffer);
     
-    fbo.allocate(videoWidth, videoHeight, GL_RGBA, 0);
-    fboBGRA.allocate(videoWidth, videoHeight, GL_RGBA, 0);
+    initShader(shader);
+    initFbo(fbo, videoWidth, videoHeight);
+    
+    fboRGBA.allocate(videoWidth, videoHeight, GL_RGBA, 0);
 }
 
 void GLVideoWriter::setFPS(float fps) {
@@ -93,24 +102,163 @@ float GLVideoWriter::getFPS() {
 }
 
 //------------------------------------------------------------------------- swizzle shader.
-void GLVideoWriter::initShader() {
-    program = glCreateProgram();
+void GLVideoWriter::initFbo(Fbo & fbo, int w, int h) const {
+    Texture & tex = fbo.tex;
+
+    fbo.fboW = w;
+    fbo.fboH = h;
+    tex.texW = NextPow2(w);
+    tex.texH = NextPow2(h);
+    tex.texT = fbo.fboW / (float)tex.texW;
+    tex.texU = fbo.fboH / (float)tex.texH;
+    tex.bExternal = false;
+    tex.bAllocated = false;
+
+    glGenFramebuffers(1, &fbo.framebuffer);
     
-    shaderVert = compileShader(GL_VERTEX_SHADER, swizzleVertexShader);
-    shaderFrag = compileShader(GL_FRAGMENT_SHADER, swizzleFragmentShader);
+    bindFbo(fbo);
+
+    initFboTexture(fbo);
     
-    glBindAttribLocation(program, 0, "position");
-    glBindAttribLocation(program, 1, "color");
-    glBindAttribLocation(program, 2, "normal");
-    glBindAttribLocation(program, 3, "texcoord");
+    bindDefaultFbo(fbo);
     
-    glAttachShader(program, shaderVert);
-    glAttachShader(program, shaderFrag);
-    
-    glLinkProgram(program);
+    fbo.bAllocated = true;
 }
 
-GLuint GLVideoWriter::compileShader(GLenum type, string source) {
+void GLVideoWriter::killFbo(Fbo & fbo) const {
+    if(fbo.bAllocated) {
+        glDeleteFramebuffers(1, &fbo.framebuffer);
+        fbo = Fbo();
+    }
+    killTexture(fbo.tex);
+}
+
+void GLVideoWriter::initFboTexture(Fbo & fbo) const {
+    Texture & tex = fbo.tex;
+
+    killTexture(tex);
+
+    tex.texT = fbo.fboW / (float)tex.texW;
+    tex.texU = fbo.fboH / (float)tex.texH;
+    
+    glGenTextures(1, &tex.texture);
+    
+    initTexture(tex);
+    
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex.texture, 0);
+    
+    tex.bExternal = false;
+    tex.bAllocated = true;
+}
+
+void GLVideoWriter::initFboTexture(Fbo & fbo, GLuint externalTexture) const {
+    Texture & tex = fbo.tex;
+
+    killTexture(tex);
+
+    tex.texT = 1.0;
+    tex.texU = 1.0;
+    tex.texture = externalTexture;
+
+    initTexture(tex);
+    
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, tex.texture, 0);
+    
+    tex.bExternal = true;
+    tex.bAllocated = true;
+}
+
+void GLVideoWriter::initTexture(const Texture & tex) const {
+
+    glBindTexture(GL_TEXTURE_2D, tex.texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex.texW, tex.texH, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void GLVideoWriter::killTexture(Texture & tex) const {
+    if(tex.bAllocated && !tex.bExternal) {
+        glDeleteTextures(1, (GLuint *)&tex.texture);
+        tex = Texture();
+    }
+}
+
+void GLVideoWriter::bindFbo(const Fbo & fbo) const {
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo.framebuffer);
+}
+
+void GLVideoWriter::bindDefaultFbo(const Fbo & fbo) const {
+    glBindFramebuffer(GL_FRAMEBUFFER, defaultFrameBuffer);
+}
+
+void GLVideoWriter::beginFbo(const Fbo & fbo) const {
+    
+//    (ofRectangle) nativeViewport = {
+//        position = (x = 0, y = 0, z = 0)
+//        x = 0x000000016f5a8b28
+//        y = 0x000000016f5a8b2c
+//        width = 640
+//        height = 1136
+//    }
+//    glViewport(nativeViewport.x,nativeViewport.y,nativeViewport.width,nativeViewport.height);
+
+//    ofGLProgrammableRenderer::setupScreenPerspective
+//    - calc perspective matrix.
+//    - calc modelview matrix.
+//    - upload modelViewProjectionMatrix to shader.
+
+    bindFbo( fbo );
+    
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void GLVideoWriter::endFbo(const Fbo & fbo) const {
+    
+    bindDefaultFbo( fbo );
+}
+
+//------------------------------------------------------------------------- swizzle shader.
+void GLVideoWriter::initShader(Shader & shader) const {
+
+    killShader(shader);
+    
+    shader.program = glCreateProgram();
+    shader.vert = compileShader(GL_VERTEX_SHADER, swizzleVertexShader);
+    shader.frag = compileShader(GL_FRAGMENT_SHADER, swizzleFragmentShader);
+    
+    glBindAttribLocation(shader.program, 0, "position");
+    glBindAttribLocation(shader.program, 1, "color");
+    glBindAttribLocation(shader.program, 2, "normal");
+    glBindAttribLocation(shader.program, 3, "texcoord");
+    
+    glAttachShader(shader.program, shader.vert);
+    glAttachShader(shader.program, shader.frag);
+    
+    glLinkProgram(shader.program);
+    
+    shader.bAllocated = true;
+}
+
+void GLVideoWriter::killShader(Shader & shader) const {
+    if(shader.bAllocated) {
+    
+        glDetachShader(shader.program, shader.vert);
+        glDeleteShader(shader.vert);
+        
+        glDetachShader(shader.program, shader.frag);
+        glDeleteShader(shader.frag);
+        
+        glDeleteProgram(shader.program);
+        
+        shader = Shader();
+    }
+}
+
+GLuint GLVideoWriter::compileShader(GLenum type, string source) const {
     GLuint shader = glCreateShader(GL_VERTEX_SHADER);
     const char * sptr = source.c_str();
     int ssize = (int)source.size();
@@ -119,8 +267,8 @@ GLuint GLVideoWriter::compileShader(GLenum type, string source) {
     return shader;
 }
 
-void GLVideoWriter::bindShader() {
-    glUseProgram(program);
+void GLVideoWriter::bindShader(const Shader & shader) const {
+    glUseProgram(shader.program);
     
     ofMatrix4x4 modelViewMatrix = ofGetCurrentMatrix(OF_MATRIX_MODELVIEW);
     ofMatrix4x4 projectionMatrix = ofGetCurrentMatrix(OF_MATRIX_PROJECTION);
@@ -130,7 +278,7 @@ void GLVideoWriter::bindShader() {
     glUniformMatrix4fv(0, 1, GL_FALSE, modelViewProjectionMatrix.getPtr());
 }
 
-void GLVideoWriter::unbindShader() {
+void GLVideoWriter::unbindShader(const Shader & shader) const {
     glUseProgram(0);
 }
 
@@ -145,11 +293,11 @@ void GLVideoWriter::draw(ofRectangle & rect) {
 }
 
 void GLVideoWriter::draw(float x, float y) {
-    draw(x, y, fbo.getWidth(), fbo.getHeight());
+    draw(x, y, fboRGBA.getWidth(), fboRGBA.getHeight());
 }
 
 void GLVideoWriter::draw(float x, float y, float width, float height) {
-    fbo.draw(x, y, width, height);
+    fboRGBA.draw(x, y, width, height);
 }
 
 //------------------------------------------------------------------------- record api.
@@ -216,8 +364,8 @@ void GLVideoWriter::initTextureCache() {
     unsigned int textureCacheID = [videoWriter textureCacheID];
     int textureCacheTarget = [videoWriter textureCacheTarget];
     
-    int textureW = fbo.getWidth();
-    int textureH = fbo.getHeight();
+    int textureW = fboRGBA.getWidth();
+    int textureH = fboRGBA.getHeight();
     
     ofTexture texture;
     texture.allocate(textureW, textureH, GL_RGBA);
@@ -230,15 +378,10 @@ void GLVideoWriter::initTextureCache() {
     texture.setUseExternalTextureID(textureCacheID);
     texture.setTextureMinMagFilter(GL_LINEAR, GL_LINEAR);
     texture.setTextureWrap(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
-    if(!ofIsGLProgrammableRenderer()) {
-        texture.bind();
-        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-        texture.unbind();
-    }
     
-    fbo.bind();
-    fbo.attachTexture(texture, GL_RGBA, 0);
-    fbo.unbind();
+    fboRGBA.bind();
+    fboRGBA.attachTexture(texture, GL_RGBA, 0);
+    fboRGBA.unbind();
 }
 
 void GLVideoWriter::killTextureCache() {
@@ -247,53 +390,31 @@ void GLVideoWriter::killTextureCache() {
     }
     bUseTextureCache = false;
     
-    int textureW = fbo.getWidth();
-    int textureH = fbo.getHeight();
+    int textureW = fboRGBA.getWidth();
+    int textureH = fboRGBA.getHeight();
     
     ofTexture texture;
     texture.allocate(textureW, textureH, GL_RGBA);
     
-    fbo.bind();
-    fbo.attachTexture(texture, GL_RGBA, 0);
-    fbo.unbind();
+    fboRGBA.bind();
+    fboRGBA.attachTexture(texture, GL_RGBA, 0);
+    fboRGBA.unbind();
 }
 
 //------------------------------------------------------------------------- begin / end.
 void GLVideoWriter::begin() {
-    fbo.begin();
+    fboRGBA.begin();
 
     ofClear(0, 255);
 }
 
 void GLVideoWriter::end() {
-    fbo.end();
+    fboRGBA.end();
 
-    //----------------------------------------------
-    if((videoWriter == nil) ||
-       [videoWriter isWriting] == NO) {
+    if((videoWriter == nil) || [videoWriter isWriting] == NO) {
         return;
     }
     
-    //----------------------------------------------
-    bool bSwizzle = (bUseTextureCache == false);
-    /*
-     *  texture caching automatically converts the RGB textures to BGR.
-     *  but if texture caching is not being used, we have to do this using
-     *  swizzle shaders.
-     */
-    
-    if(bSwizzle) {
-        
-        bindShader();
-        fboBGRA.begin();
-        
-        fbo.draw(0, 0);
-        
-        fboBGRA.end();
-        unbindShader();
-    }
-    
-    //---------------------------------------------- frame time.
     CMTime frameTime = kCMTimeZero;
     
     if(bLockToFPS) {
@@ -303,19 +424,9 @@ void GLVideoWriter::end() {
         frameTime = CMTimeMakeWithSeconds(time, NSEC_PER_SEC);
     }
     
-    //---------------------------------------------- add video frame.
-    if(bSwizzle) {
-        fboBGRA.bind();
-    }
-
-    //ofLog() << "show do wrignit " << recordFrameNum << ", " << frameTime.value;
     BOOL bVideoFrameAdded = [videoWriter addFrameAtTime:frameTime];
     if(bVideoFrameAdded == YES) {
         recordFrameNum += 1;
-    }
-    
-    if(bSwizzle) {
-        fboBGRA.unbind();
     }
 }
 
